85c72fa68470344185adcaf90bb2fa99428e7e6f
Nexiy <inkstand_vip@163.com>
2020-12-23 14:29:38 +0800

    调整答题结束页面样式，新增可以点击按钮

--- src/views/finished/index.vue
+++ src/views/finished/index.vue
@@ -14,9 +14,11 @@
 			<img src="../../assets/img/finished.png" alt="">
 			<h1>恭喜测评完成！</h1>
 			<div class="rule">
-				<p>你的测评结果已经提交，我们的专家团队正在进行专业分析，通过精准专家解读，为你提供详细的个性化测评报告。敬请期待～</p>
+				<p>你的测评结果已经提交，我们的专家团队正在进行专业分析，通过精准专家解读，为你提供详细的个性化测评报告。</p>
 			</div>
+			<a href="http://www.ruggear.mobi/tianshengwocai/#/report" class="button-default">查看报告</a>
 		</div>
+		
 	</section>
 </template>
 <script>
